511d484fd8a74b2ceff6097d08dac96c6d2bbf67
Matthew MacKinnon <matt@Matthews-MacBook-Pro.local>
2018-04-24 15:16:09 -0400

    updated based on EE suggestions in git comments

--- spec/features/01_view_categories_show_spec.rb
+++ spec/features/01_view_categories_show_spec.rb
@@ -3,19 +3,16 @@ require 'rails_helper'
 
 feature 'user views a category show page' do
   let! (:category){Category.create!(name: "Bigfoots")}
-  #let! (:cryptid){Cryptid.create!(name: "Bigfoot", category: category)}
   scenario 'user sees cryptids in that category' do
     visit "/categories/#{category.id}"
 
     expect(page).to have_content(category.name)
-    # expect(page).to have_link(cryptid.name)
   end
 
   scenario "category show page contains recently spotted section" do
     visit "/categories/#{category.id}"
 
     expect(page).to have_content('Recently Spotted')
-    # will add additional tests should feature expand
   end
 
 end
