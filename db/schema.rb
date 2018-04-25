93709158fd550367a5b8a6a73f0b434e76d9a454
Evan duBois <duboisevan@gmail.com>
2018-04-25 15:29:22 -0400

    ran migrations for codeship

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_175958) do
+ActiveRecord::Schema.define(version: 2018_04_25_180942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cryptids", force: :cascade do |t|
     t.string "name", null: false
@@ -21,12 +27,6 @@ ActiveRecord::Schema.define(version: 2018_04_25_175958) do
     t.text "description", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_cryptids_on_user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "regions", force: :cascade do |t|
