0a612abf1497d55d5bdcb19ca4368edfc0851244
Matthew MacKinnon <matt@Matthews-MacBook-Pro.local>
2018-04-25 14:25:58 -0400

    updated per EE request - deleted empty files and corrected seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_155200) do
+ActiveRecord::Schema.define(version: 2018_04_25_180942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
