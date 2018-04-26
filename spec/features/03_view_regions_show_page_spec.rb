c9d2a90128c3308a365abe151c8c19091b914fa8
Matthew MacKinnon <matt@Matthews-MacBook-Pro.local>
2018-04-26 15:17:57 -0400

    savepoint for merging latest pull request

---
+++ spec/features/03_view_regions_show_page_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+
+feature 'user views a category show page' do
+  let!(:manape) {Category.create!(name: "Hairy Man Apes")}
+  let!(:test_user) { FactoryBot.create(:user) }
+  let!(:usa) {Region.create!(name: "USA")}
+  let!(:bigfoot) do
+     Cryptid.create!(
+      name: "Bigfoot",
+      description: "Hairy and scary",
+      user: test_user,
+      region: usa,
+      category: manape)
+  end
+  let!(:wendigo) do
+     Cryptid.create!(
+      name: "Wendigo",
+      description: "Hairier and scarier",
+      user: test_user,
+      region: usa,
+      category: manape)
+  end
+
+  scenario 'user sees cryptids in that category' do
+    visit "/regions/#{usa.id}"
+
+    expect(page).to have_content(usa.name)
+    expect(page).to have_link(bigfoot.name)
+    expect(page).to have_link(wendigo.name)
+  end
+
+end
