51876ee1d790aed65afc9e674fe3d90cc68f5198
Matthew MacKinnon <matt@Matthews-MacBook-Pro.local>
2018-04-26 15:17:44 -0400

    savepoint for merging latest pull request

--- app/controllers/cryptids_controller.rb
+++ app/controllers/cryptids_controller.rb
@@ -2,4 +2,21 @@ class CryptidsController < ApplicationController
   def show
     @cryptid = Cryptid.find(params[:id])
   end
+
+  def new
+    @cryptid = Cryptid.new
+    @regions = Region.all
+    @categories = Category.all
+    end
+
+  def create
+    @cryptid = Cryptid.new(cryptid_params)
+    @region = Region.where()
+  end
+
+private
+
+  def cryptid_params
+    params.require(:cryptid).permit(:name, :pic_url, :description)
+  end
 end
