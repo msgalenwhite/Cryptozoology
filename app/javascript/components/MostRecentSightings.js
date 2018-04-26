cf1b9e43dec45006fada369dd6a60ad117cd70e0
Galen <galen.h.white@gmail.com>
2018-04-26 15:25:43 -0400

    savepoint

--- app/javascript/components/MostRecentSightings.js
+++ app/javascript/components/MostRecentSightings.js
@@ -10,21 +10,21 @@ class MostRecentSightings extends Component {
   }
 
   componentDidMount() {
-    fetch("/api/vi/sightings")
-      .then ( response => {
-        if ( response.ok ) {
-          return response;
-        } else {
-          let errorMessage = `${response.status} (${response.statusText})`;
-          let error = new Error(errorMessage);
-          throw(error);
-        }
-      })
-      .then ( response => response.json() )
-      .then ( response => {
-        console.log(response)
-      })
-      .catch ( error => console.error(`Error in fetch: ${error.message}`) );
+    // fetch("/api/vi/sightings")
+    //   .then ( response => {
+    //     if ( response.ok ) {
+    //       return response;
+    //     } else {
+    //       let errorMessage = `${response.status} (${response.statusText})`;
+    //       let error = new Error(errorMessage);
+    //       throw(error);
+    //     }
+    //   })
+    //   .then ( response => response.json() )
+    //   .then ( response => {
+    //     console.log(response)
+    //   })
+    //   .catch ( error => console.error(`Error in fetch: ${error.message}`) );
   }
 
   render(){
