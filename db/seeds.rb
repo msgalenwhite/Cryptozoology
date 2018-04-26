8f66a9ad4059dbc4d5de663d518a26859f77d22f
Galen <galen.h.white@gmail.com>
2018-04-26 10:34:11 -0400

    added faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-lake_monsters = Category.create!(name: "Lake Monsters")
-hairy_hominids = Category.create!(name: "Hairy Hominids")
-carnivorous_mammal = Category.create!(name: "Carnivorous Mammal")
-reptile = Category.create!(name: "Reptile/Dinosaur")
+require 'faker'
+
+lake_monsters = Category.find_or_create_by(name: "Lake Monsters")
+hairy_hominids = Category.find_or_create_by(name: "Hairy Hominids")
+carnivorous_mammal = Category.find_or_create_by(name: "Carnivorous Mammal")
+reptile = Category.find_or_create_by(name: "Reptile/Dinosaur")
+
+20.times do
+  User.find_or_create_by(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    encrypted_password: "thisisapassword"
+  )
+end
