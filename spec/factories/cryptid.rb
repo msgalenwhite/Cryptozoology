5cf9681e757de3f149eb60861bd13ad6d9c14694
Galen <galen.h.white@gmail.com>
2018-04-27 15:31:32 -0400

    added factorybot associations for database changes

--- spec/factories/cryptid.rb
+++ spec/factories/cryptid.rb
@@ -4,6 +4,9 @@ FactoryBot.define do
   factory :cryptid do
     name 'name'
     description 'description'
+
     association :user, factory: :user
+    association :region, factory: :region
+    association :category, factory: :category
   end
 end
