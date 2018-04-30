fdbd9615f7bb54bb64e68ac012e009150a1861bd
Galen <galen.h.white@gmail.com>
2018-04-30 13:33:31 -0400

    have to walk away

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,24 @@
+CarrierWave.configure do |config|
+  if !Rails.env.test?
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    }
+    config.fog_directory  = ENV["S3_BUCKET"]
+  end
+  # For testing, upload files to local `tmp` folder.
+    if Rails.env.test? || Rails.env.cucumber?
+      config.storage = :file
+      config.enable_processing = false
+      config.root = "#{Rails.root}/tmp"
+    else
+      config.storage = :fog
+    end
+
+    config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+
+    config.fog_directory    = ENV['S3_BUCKET_NAME']
+    config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+    config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+  end
