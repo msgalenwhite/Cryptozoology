13a2f61b69d23d949edc9a83a8dd8571c53cded6
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-01 11:13:27 -0400

    missed file

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,9 +1,5 @@
 class SightingSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
   attributes :id, :location, :description, :cryptid_pic, :pic_url, :rating, :created_at, :user_name, :cryptid_name
-=======
-  attributes :id, :location, :description, :pic_url, :rating, :created_at, :user_name, :cryptid_name
->>>>>>> 6fcd09a45cb171e64a5d97019d37853a0610bef4
 
   def user_name
     object.user.name
