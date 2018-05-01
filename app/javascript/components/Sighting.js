9530ea57c1a4b01b4a108b8cb5bf7ba3ba2750bb
Galen <galen.h.white@gmail.com>
2018-05-01 11:46:54 -0400

    changes

--- app/javascript/components/Sighting.js
+++ app/javascript/components/Sighting.js
@@ -3,6 +3,9 @@ import RatingBar from './RatingBar'
 import iconData from '../constants/iconData'
 
 const Sighting = props => {
+  let icons = iconData["confidence"].map((icon) => {
+    
+  })
 
   return(
     <div className='sighting-tile row'>
@@ -12,10 +15,10 @@ const Sighting = props => {
         <div className='date'>(Sighted: {props.created_at})</div>
       </div>
       <div className='columns small-7 right-column'>
-        <RatingBar
-          headerText='Confidence in Sighting:'
-          icon={iconData["confidence"][props.rating]}
-        />
+        <div>
+          Confidence in Sighting:
+          {icons}
+        </div>
         <div>{props.cryptid_name}</div>
         <div>Sighted at: {props.location}</div>
         <p>{props.description}</p>
